921f1fba26e77f6fef48ca0e9a491874ef1ce879
qurriahSam <mugirethe1@gmail.com>
2024-06-13 20:52:21 +0300

    create register and login pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './modules/auth/register/register.component';
+import { LoginComponent } from './modules/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent}
+];
