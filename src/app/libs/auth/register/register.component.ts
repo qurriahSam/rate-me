61fd16ea4fe23897148fb25a248a1d0ecd988e90
qurriahSam <mugirethe1@gmail.com>
2024-06-18 18:24:02 +0300

    feat: add form controls

--- src/app/libs/auth/register/register.component.ts
+++ src/app/libs/auth/register/register.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    email: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
 
+  handleSubmit() {
+    console.log(this.registerForm.value);
+  }
 }
