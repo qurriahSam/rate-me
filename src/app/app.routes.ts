6cfa3872c5dc88f1fe0e7ea104ec668f78153e5c
qurriahSam <mugirethe1@gmail.com>
2024-07-16 16:13:15 +0300

    feat: create dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './libs/auth/register/register.component';
 import { LoginComponent } from './libs/auth/login/login.component';
+import { DashboardComponent } from './libs/home/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
