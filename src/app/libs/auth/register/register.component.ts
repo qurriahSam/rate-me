2d61fb74eebe7cb4ad69a3fd92ac7f51305df24b
qurriahSam <mugirethe1@gmail.com>
2024-07-16 15:22:43 +0300

    feat: use angular validators to check form errors

--- src/app/libs/auth/register/register.component.ts
+++ src/app/libs/auth/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 import { RegisterAction } from '../../../store/auth/register-actions';
@@ -19,59 +25,43 @@ import { CommonModule } from '@angular/common';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  registerForm = new FormGroup({
-    email: new FormControl({ value: '', disabled: false }),
-    username: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-  });
+  registerForm: FormGroup;
   isLoading$: Observable<boolean>;
   regError$: Observable<string | null>;
 
-  constructor(private store: Store<AppStateInterface>) {
+  constructor(
+    private store: Store<AppStateInterface>,
+    private fb: FormBuilder
+  ) {
     this.isLoading$ = store.pipe(select(registerIsLoadingSelector));
     this.regError$ = store.pipe(select(registerErrorSelector));
+
+    this.registerForm = this.fb.group(
+      {
+        username: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+      },
+      { validator: this.passwordMatchValidator }
+    );
   }
 
   handleSubmit() {
     console.log(this.registerForm.value);
-
-    this.store.dispatch(
-      RegisterAction.registerUser({
-        email: this.registerForm.value.email as string,
-        password: this.registerForm.value.password as string,
-      })
-    );
+    if (this.registerForm.valid) {
+      this.store.dispatch(
+        RegisterAction.registerUser({
+          email: this.registerForm.value.email as string,
+          password: this.registerForm.value.password as string,
+        })
+      );
+    }
   }
 
-  validateEmail(email: any) {
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailPattern.test(email);
-  }
-
-  validatePassword(password: any) {
-    const minLength = 8;
-    const hasUpperCase = /[A-Z]/.test(password);
-    const hasLowerCase = /[a-z]/.test(password);
-    const hasNumber = /\d/.test(password);
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-
-    if (password.length < minLength) {
-      return false;
-    }
-    if (!hasUpperCase) {
-      return false;
-    }
-    if (!hasLowerCase) {
-      return false;
-    }
-    if (!hasNumber) {
-      return false;
-    }
-    if (!hasSpecialChar) {
-      return false;
-    }
-
-    return true;
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value
+      ? null
+      : { mismatch: true };
   }
 }
