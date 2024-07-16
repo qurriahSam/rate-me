20655ff8ba86abbd48e32e6c6b7f3d6aae8a3929
qurriahSam <mugirethe1@gmail.com>
2024-07-16 09:31:59 +0300

    feat: form input validity check

--- src/app/libs/auth/register/register.component.ts
+++ src/app/libs/auth/register/register.component.ts
@@ -5,7 +5,10 @@ import { Store, select } from '@ngrx/store';
 import { RegisterAction } from '../../../store/auth/register-actions';
 import { AppStateInterface } from '../../../models/appState.interface';
 import { Observable } from 'rxjs';
-import { registerIsLoadingSelector } from '../../../store/auth/registerSelector';
+import {
+  registerErrorSelector,
+  registerIsLoadingSelector,
+} from '../../../store/auth/registerSelector';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -17,15 +20,17 @@ import { CommonModule } from '@angular/common';
 })
 export class RegisterComponent {
   registerForm = new FormGroup({
-    email: new FormControl(''),
+    email: new FormControl({ value: '', disabled: false }),
     username: new FormControl(''),
     password: new FormControl(''),
     confirmPassword: new FormControl(''),
   });
   isLoading$: Observable<boolean>;
+  regError$: Observable<string | null>;
 
   constructor(private store: Store<AppStateInterface>) {
     this.isLoading$ = store.pipe(select(registerIsLoadingSelector));
+    this.regError$ = store.pipe(select(registerErrorSelector));
   }
 
   handleSubmit() {
@@ -33,9 +38,40 @@ export class RegisterComponent {
 
     this.store.dispatch(
       RegisterAction.registerUser({
-        email: 'mail2@g.com',
-        password: 'password22',
+        email: this.registerForm.value.email as string,
+        password: this.registerForm.value.password as string,
       })
     );
+  }
+
+  validateEmail(email: any) {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
+
+  validatePassword(password: any) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    if (password.length < minLength) {
+      return false;
+    }
+    if (!hasUpperCase) {
+      return false;
+    }
+    if (!hasLowerCase) {
+      return false;
+    }
+    if (!hasNumber) {
+      return false;
+    }
+    if (!hasSpecialChar) {
+      return false;
+    }
+
+    return true;
   }
 }
