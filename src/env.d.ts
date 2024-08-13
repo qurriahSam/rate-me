d9c4cf5bca14321546632ee197fe41ca9c60fae6
qurriahSam <mugirethe1@gmail.com>
2024-08-13 13:33:07 +0300

    fix(firebase): use env file for environments

---
+++ src/env.d.ts
@@ -0,0 +1,21 @@
+// Define the type of the environment variables.
+declare interface Env {
+  readonly NODE_ENV: string;
+  // Replace the following with your own environment variables.
+  // Example: NGX_VERSION: string;
+  NG_APP_API_KEY: string;
+  NG_APP_AUTH_DOMAIN: string;
+  NG_APP_PROJECT_ID: string;
+  NG_APP_STORAGE_BUCKET: string;
+  NG_APP_MESSAGING_SENDER_ID: string;
+  NG_APP_APP_ID: string;
+  NG_APP_MEASURMENT_ID: string;
+}
+
+// Choose how to access the environment variables.
+// Remove the unused options.
+
+// 1. Use import.meta.env.YOUR_ENV_VAR in your code. (conventional)
+declare interface ImportMeta {
+  readonly env: Env;
+}
