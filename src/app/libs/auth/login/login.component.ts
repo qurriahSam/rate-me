b9f0d7f60ac3f29ea2bf89b5509bcc7d25aa4b1c
qurriahSam <mugirethe1@gmail.com>
2024-10-15 18:10:17 +0300

    feat: store user data in local storage

--- src/app/libs/auth/login/login.component.ts
+++ src/app/libs/auth/login/login.component.ts
@@ -1,17 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   ReactiveFormsModule,
   FormBuilder,
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { AppStateInterface } from '../../../models/appState.interface';
 import { AuthAction } from '../../../store/auth/actions';
-import { authIsLoadingSelector } from '../../../store/auth/authSelectors';
+import {
+  authIsLoadingSelector,
+  registerUserSelector,
+} from '../../../store/auth/authSelectors';
 import { Observable } from 'rxjs';
+import { User } from '../../../models/loggedUser';
+import { LocalStorageService } from '../../../localStorageService/local-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -20,18 +25,35 @@ import { Observable } from 'rxjs';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   isLoading$: Observable<boolean>;
+  regUser$: Observable<User>;
+
   constructor(
     private fb: FormBuilder,
-    private store: Store<AppStateInterface>
+    private store: Store<AppStateInterface>,
+    private localStorage: LocalStorageService,
+    private router: Router
   ) {
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
     });
     this.isLoading$ = store.pipe(select(authIsLoadingSelector));
+    this.regUser$ = store.pipe(select(registerUserSelector));
+  }
+
+  ngOnInit(): void {
+    this.regUser$.subscribe((user) => {
+      if (user.id) {
+        this.localStorage.setItem('id', user.id);
+        if (user.email) {
+          this.localStorage.setItem('email', user.email);
+        }
+        this.router.navigate(['/dashboard']);
+      }
+    });
   }
 
   handleSubmit() {
