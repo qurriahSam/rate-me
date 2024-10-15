b173d88e018c50882a80fdfc722596e4cf8939d5
qurriahSam <mugirethe1@gmail.com>
2024-10-15 18:07:18 +0300

    feat: load user details from local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AlertComponent } from './libs/alert/alert.component';
 import { LocalStorageService } from './localStorageService/local-storage.service';
+import { Store } from '@ngrx/store';
+import { AppStateInterface } from './models/appState.interface';
+import { AuthAction } from './store/auth/actions';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +15,17 @@ import { LocalStorageService } from './localStorageService/local-storage.service
 })
 export class AppComponent implements OnInit {
   title = 'chat-grp-ai';
-  constructor(private localStorage: LocalStorageService) {}
+  constructor(
+    private localStorage: LocalStorageService,
+    private store: Store<AppStateInterface>
+  ) {}
   ngOnInit(): void {
-    const user = this.localStorage.getItem('user');
-    if (user) {
+    const id = this.localStorage.getItem('id');
+    const email = this.localStorage.getItem('email');
+    if (id && email) {
+      this.store.dispatch(
+        AuthAction.registrationSuccess({ id: id, email: email })
+      );
     }
   }
 }
