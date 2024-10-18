2ed6c4eda6d358980f66c36b761514b394e79ffe
qurriahSam <mugirethe1@gmail.com>
2024-10-18 21:30:02 +0300

    feat: set landingpage as default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './libs/auth/register/register.component';
 import { LoginComponent } from './libs/auth/login/login.component';
 import { DashboardComponent } from './libs/home/dashboard/dashboard.component';
+import { LandingPageComponent } from './libs/landing-page/landing-page.component';
 
 export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
