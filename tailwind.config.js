bdafca91e70e79455a3784b012481d8eba005359
qurriahSam <mugirethe1@gmail.com>
2024-10-29 19:27:12 +0300

    chore: themes setting for daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [require("daisyui")],
   daisyui: {
     themes: ["emerald", "dim"],
   },
-}
-
+};
