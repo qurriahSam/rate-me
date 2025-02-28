7903650de34c329d2b9be8895a007c6273f87b13
qurriahSam <mugirethe1@gmail.com>
2025-02-28 15:12:34 +0300

    feat: create guarded route for review page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,17 +5,25 @@ import { DashboardComponent } from './libs/home/dashboard/dashboard.component';
 import { LandingPageComponent } from './libs/landing-page/landing-page.component';
 import { MyProjectsComponent } from './libs/home/libs/my-projects/my-projects.component';
 import { AllProjectsComponent } from './libs/home/libs/all-projects/all-projects.component';
+import { ReviewPageComponent } from './libs/review-page/review-page.component';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   {
+    path: 'review/:_id',
+    component: ReviewPageComponent,
+    canActivate: [authGuard],
+  },
+  {
     path: 'dashboard',
     component: DashboardComponent,
     children: [
       { path: 'my-projects', component: MyProjectsComponent },
       { path: 'all-projects', component: AllProjectsComponent },
+
       { path: '', redirectTo: 'all-projects', pathMatch: 'full' },
     ],
   },
