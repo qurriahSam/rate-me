671a9265d3987df0074f24e076ace754d7680662
qurriahSam <mugirethe1@gmail.com>
2025-02-28 15:10:53 +0300

    refactor: remove modal

--- src/app/libs/home/libs/vote-card/vote-card.component.ts
+++ src/app/libs/home/libs/vote-card/vote-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../../../../models/project';
+import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-vote-card',
-    imports: [],
-    templateUrl: './vote-card.component.html',
-    styleUrl: './vote-card.component.css'
+  selector: 'app-vote-card',
+  imports: [RouterLink],
+  templateUrl: './vote-card.component.html',
+  styleUrl: './vote-card.component.css',
 })
 export class VoteCardComponent {
   @Input() project!: Project;
