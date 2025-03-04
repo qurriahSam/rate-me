063ce2317f63949c4798189cb027c16dda8af7a4
qurriahSam <mugirethe1@gmail.com>
2025-03-04 16:14:41 +0300

    refactor: implement half-star rating system and calculate average rating in vote card component

--- src/app/libs/home/libs/vote-card/vote-card.component.ts
+++ src/app/libs/home/libs/vote-card/vote-card.component.ts
@@ -11,8 +11,17 @@ import { RouterLink } from '@angular/router';
 export class VoteCardComponent implements OnInit {
   @Input() project!: Project;
   description?: string;
+  avarageRating?: number;
   ngOnInit(): void {
     console.log(this.project);
     this.description = this.project.description.slice(0, 100) + '...';
+    let rating = this.project.ratings.reduce(
+      (acc, curr) => acc + Number(curr.rate),
+      0
+    );
+    this.avarageRating = this.customRound(rating / this.project.ratings.length);
+  }
+  customRound(num: number): number {
+    return Math.floor(num) + (num - Math.floor(num) >= 0.5 ? 0.5 : 0);
   }
 }
