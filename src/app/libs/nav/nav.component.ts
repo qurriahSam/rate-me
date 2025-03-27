9cd54c7f8a9c234b04414d944d54fe32c9ffe6ce
qurriahSam <mugirethe1@gmail.com>
2025-03-27 15:39:54 +0300

    feat: enhance navigation links with active state management

--- src/app/libs/nav/nav.component.ts
+++ src/app/libs/nav/nav.component.ts
@@ -2,25 +2,27 @@ import { Component } from '@angular/core';
 import { AppStateInterface } from '../../models/appState.interface';
 import { select, Store } from '@ngrx/store';
 import { registerUserSelector } from '../../store/auth/authSelectors';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { User } from '../../models/loggedUser';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
 import { AuthAction } from '../../store/auth/actions';
 import { LocalStorageService } from '../../services/localStorageService/local-storage.service';
 
 @Component({
   selector: 'app-nav',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   user$: Observable<User>;
+  activeLink: string = 'all-projects';
 
   constructor(
     private store: Store<AppStateInterface>,
-    private localStorage: LocalStorageService
+    private localStorage: LocalStorageService,
+    private route: ActivatedRoute
   ) {
     this.user$ = store.pipe(select(registerUserSelector));
   }
